e6a5f13eb1302821b3e29815b2cb295a1178f263
SavithaPin <prc-lh138-04@PRC-LH138-04s-iMac.local>
2016-07-14 14:50:12 -0500

    Hello App

--- SavithasApp/ViewController.swift
+++ SavithasApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var Input: UITextField!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
